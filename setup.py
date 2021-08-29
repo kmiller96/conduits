5d6c0067bd884e016135e59850a2198721a4c716
Kale Miller <kale.miller@prometheusai.com.au>
2021-08-29 12:18:28 +0000

    Added install dependencies to the setup.py package

--- setup.py
+++ setup.py
@@ -12,4 +12,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
+    install_requires=["pandas", "networkx[default]"],
 )
