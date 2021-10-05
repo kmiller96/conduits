a4708a6a47ac186a4628d7ea156e232523bfee73
Kale Miller <km@arcx.money>
2021-10-05 05:03:40 +0000

    Incremented version to trigger rebuild on PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="conduits",
-    version="0.1.0",
+    version="0.1.1",
     author="Kale Miller",
     url="https://github.com/kmiller96/conduits",
     long_description=long_description,
